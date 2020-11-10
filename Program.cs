2f7b75d8b45fb12efdb88cad7b9b21d7b3979312
filipqnicki <filipqnicki@gmail.com>
2020-11-10 16:35:14 +0100

    fix txt name load data from txt file add start menu add function round()

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TheHangman
 {
     class Program
     {
+
+        public struct countryAndCapitalStruct
+        {
+            public string country { get; set; }
+            public string capital { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line;
+            string[] words = new string[2];
+            var countryAndCapitalList = new List<countryAndCapitalStruct>();
+            var box = new countryAndCapitalStruct();
+            ConsoleKeyInfo choice;
+            
+            System.IO.StreamReader file = new System.IO.StreamReader(@"E:\Projekty\TheHangmanGame\TheHangman\resources\countries_and_capitals.txt");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                words = line.Split('|');
+                box.country = words[0];
+                box.capital = words[1];
+                countryAndCapitalList.Add(box);
+            }
+ 
+            Console.WriteLine("The game Hangman");
+            Console.WriteLine("\nWould you like to start? (press s to start, e to exit)");
+            do
+            {
+                choice = Console.ReadKey(true);
+                if (choice.KeyChar == 's' ^ choice.KeyChar == 'S') round();
+            } while (choice.KeyChar != 'e' && choice.KeyChar != 'E'); 
+
         }
+
+        private static void round()
+        {
+            Console.Clear();
+            Random rnd = new Random();
+            int randomIndex;
+            //randomIndex = rnd.Next(countryAndCapitalList.Count);
+        }
+
+
+
     }
 }
