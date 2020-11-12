121028a417dbed24f5582c1971bb4dfa017cd32e
filipqnicki <filipqnicki@gmail.com>
2020-11-12 21:54:34 +0100

    add ranking of top players add top10.txt as a file with best scores fix little change of player in game interface

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading;
 using System.Timers;
+using System.IO;
 
 namespace TheHangman
 {
@@ -31,37 +33,42 @@ namespace TheHangman
 
         static void Main(string[] args)
         {
+            string playerName;
             string line;
             string[] words;
             var countryAndCapitalList = new List<CountryAndCapitalStruct>();
-            var box = new CountryAndCapitalStruct();
+            var countryAndCapitalBox = new CountryAndCapitalStruct();
+
             ConsoleKeyInfo choice;
 
             //open files
             System.IO.StreamReader fileWithCapitals = new System.IO.StreamReader(@"E:\Projekty\TheHangmanGame\TheHangman\resources\countries_and_capitals.txt");
-            System.IO.StreamReader fileWithTop10 = new System.IO.StreamReader(@"E:\Projekty\TheHangmanGame\TheHangman\resources\top10.txt");
 
             while ((line = fileWithCapitals.ReadLine()) != null)
             {
                 words = line.Split(" | ");                                              // loading words from file to list
-                box.Country = words[0];
-                box.Capital = words[1];
-                countryAndCapitalList.Add(box);
+                countryAndCapitalBox.Country = words[0];
+                countryAndCapitalBox.Capital = words[1];
+                countryAndCapitalList.Add(countryAndCapitalBox);
             }
 
             Console.WriteLine("The game Hangman");
+            Console.WriteLine("Write your name");
+            playerName = Console.ReadLine();
             Console.WriteLine("\nWould you like to start? (press s to start, e to exit)");
 
             do
             {
                 choice = Console.ReadKey(true);
-                if (choice.KeyChar == 's' ^ choice.KeyChar == 'S') Game(countryAndCapitalList);   //start menu
+                if (choice.KeyChar == 's' ^ choice.KeyChar == 'S') Game(countryAndCapitalList, playerName);   //start menu
+
             } while (choice.KeyChar != 'e' && choice.KeyChar != 'E');
         }
 
-        private static void Game(List<CountryAndCapitalStruct> countryAndCapitalList)
+        private static void Game(List<CountryAndCapitalStruct> countryAndCapitalList, string playerName)
         {
             bool winGame = false;
+            short gameStatus = 0;
             int randomIndex;
             int lifes = 6;
             int guessedLettersNumber = 0;
@@ -71,7 +78,7 @@ namespace TheHangman
             bool[] isLetterGuessed;
             ConsoleKeyInfo choice;
             Stopwatch stopwatch = new Stopwatch();
-            TimeSpan timeSpan;
+            TimeSpan timeSpan = new TimeSpan();
 
             Console.Clear();
             Random rnd = new Random();
@@ -89,7 +96,7 @@ namespace TheHangman
 
             while (lifes > 0)
             {
-                CreateGameInterface(lifes, capitalToGuess, isLetterGuessed, wrongLetters);
+                CreateGameInterface(lifes, capitalToGuess, isLetterGuessed, wrongLetters, gameStatus);
 
                 if (winGame)
                 {
@@ -112,13 +119,11 @@ namespace TheHangman
 
                         if (CheckLetter(ref guessedLettersNumber, ref lifes, userGuess[0], capitalToGuess, isLetterGuessed, ref wrongLetters))
                         {
-                            Console.WriteLine("Well done!");
-                            Thread.Sleep(2000);
+                            gameStatus = 1;
                         }
                         else
                         {
-                            Console.WriteLine("Wrong letter!");
-                            Thread.Sleep(2000);
+                            gameStatus = 2;
                         }
                     }
                     if (choice.KeyChar == '2')
@@ -129,14 +134,12 @@ namespace TheHangman
 
                         if (String.Compare(capitalToGuess, userGuess, true) == 0)
                         {
-                            Console.WriteLine("Well done!");
-                            Thread.Sleep(2000);
+                            gameStatus = 3;
                             winGame = true;
                         }
                         else
                         {
-                            Console.WriteLine("Wrong capital!");
-                            Thread.Sleep(2000);
+                            gameStatus = 4;
                             lifes -= 2;
                         }
                     }
@@ -154,13 +157,14 @@ namespace TheHangman
                 Console.WriteLine("You lost...");
                 Console.WriteLine("You guessed " + guessedLettersNumber + " letters");
                 Console.WriteLine("The Capital that you had to guess: " + capitalToGuess);
-                Console.WriteLine("Your time: " + timeSpan.Seconds + " seconds");
+                Console.WriteLine("Your time: " + timeSpan.Seconds + " seconds\n");
             }
 
-            Console.WriteLine("\nWould you like to start? (press s to start, e to exit)");
+            ScoreBoard(playerName, timeSpan.Seconds, guessedLettersNumber, capitalToGuess);
+            Console.WriteLine("\nWould you like to start next game? (press s to start, e to exit)");
         }
 
-        private static void CreateGameInterface(int lifes, string capitalToGuess, bool[] isLetterGuessed, string wrongLetters)
+        private static void CreateGameInterface(int lifes, string capitalToGuess, bool[] isLetterGuessed, string wrongLetters, short gameStatus)
         {
             int counter = 0;
 
@@ -194,8 +198,30 @@ namespace TheHangman
                 }
                 counter++;
             }
+            
             Console.Write("\n");
+            Console.WriteLine();
             DrawHangman(lifes);
+
+            switch (gameStatus)
+            {
+                case 0:
+                    break;
+                case 1:
+                    Console.WriteLine("Well done!");
+                    break;
+                case 2:
+                    Console.WriteLine("Wrong letter!");
+                    break;
+                case 3:
+                    Console.WriteLine("Well done!");
+                    break;
+                case 4:
+                    Console.WriteLine("Wrong capital!");
+                    break;
+                default:
+                    break;
+            }
         }
 
         private static bool CheckLetter(ref int guessedLetters, ref int lifes, char letter, string word, bool[] isLetterGuessed, ref string wrongLetters)
@@ -240,6 +266,55 @@ namespace TheHangman
 
             }
             return true;
+
+        }
+
+        private static void ScoreBoard(string playerName, int seconds, int guessedLettersNumber, string capitalToGuess)
+        {
+            string line;
+            string[] data;
+            DateTime thisDay = DateTime.Now;
+            var playerStatsBox = new PlayerStats();
+            var playersStatsList = new List<PlayerStats>();
+            StreamReader fileWithTop10 = new StreamReader(@"E:\Projekty\TheHangmanGame\TheHangman\resources\top10.txt");
+
+
+            while ((line = fileWithTop10.ReadLine()) != null)
+            {
+                data = line.Split(" | ");                                              // loading words from file to list
+                playerStatsBox.Name = data[0];
+                playerStatsBox.Date = data[1];
+                playerStatsBox.GuessingTime = Int32.Parse(data[2]);
+                playerStatsBox.GuessedLettters = Int32.Parse(data[3]);
+                playerStatsBox.GuessedCapital = data[4];
+                playersStatsList.Add(playerStatsBox);
+            }
+
+            fileWithTop10.Close();
+            StreamWriter fileToSave = new StreamWriter(@"E:\Projekty\TheHangmanGame\TheHangman\resources\top10.txt", false);
+
+            playerStatsBox.Name = playerName;
+            playerStatsBox.Date = thisDay.ToString();
+            playerStatsBox.GuessingTime = seconds;
+            playerStatsBox.GuessedLettters = guessedLettersNumber;
+            playerStatsBox.GuessedCapital = capitalToGuess;
+
+            playersStatsList.Add(playerStatsBox);
+
+            List<PlayerStats> playerStatsSortedList = playersStatsList.OrderBy(playerStats => playerStats.GuessingTime).ToList();
+
+            if (playerStatsSortedList.Count == 11) playerStatsSortedList.RemoveAt(playerStatsSortedList.Count - 1);
+
+            Console.Write("\n");
+            foreach (PlayerStats playerStatsDummy in playerStatsSortedList)
+            {
+                line = (playerStatsDummy.Name + " | " + playerStatsDummy.Date + " | " + playerStatsDummy.GuessingTime
+                + " | " + playerStatsDummy.GuessedLettters + " | " + playerStatsDummy.GuessedCapital);
+
+                Console.WriteLine(line);
+                fileToSave.WriteLine(line); //nie kasuje tego, co było wcześniej
+            }
+            fileToSave.Close();
 
         }
         private static void DrawHangman(int lifes)
